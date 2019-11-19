23fc7805ac7ed95fcce02fe0074f66dcfcd9956f
himanshu1365 <himanshu1365.cse16@chitkara.edu.in>
2019-11-19 11:07:09 +0530

    Added frontend and changed mongo url

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,8 @@
 const {MONGO_HOSTNAME,MONGO_PORT, MONGO_DB} = require('./keys')
 const mongoose = require('mongoose')
 const url = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
-mongoose.connect(url,{useNewUrlParser: true, useCreateIndex: true})
+const mlaburl = 'mongodb://admin:himanshu0018@ds347298.mlab.com:47298/examportal'
+mongoose.connect(mlaburl,{useNewUrlParser: true, useCreateIndex: true})
 
 mongoose.connection.once('open',()=>{
     console.log('Mongoose Connection is Successful')
